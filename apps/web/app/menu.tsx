3c780051ff76961bb2da24f9fc8c5323d74b511b
roxanne000 <roxannekellogg29@gmail.com>
2026-09-18 16:07:58 +0000

    Create menu.tsx

---
+++ apps/web/app/menu.tsx
@@ -0,0 +1,3 @@
+export default function Menu(){
+  return <div className="pt-28 max-w-4xl mx-auto px-6 text-white bg-black min-h-screen"><h1 className="text-4xl font-bold">Menu</h1><div className="mt-8"><p>Pizza - 120 GHS</p><p>Tilapia & Banku - 150 GHS</p><p>Mojito - 45 GHS</p></div></div>
+}
