3dbc9199f4039d4dfbadd9e43e38b7b2a2974734
roxanne000 <roxannekellogg29@gmail.com>
2026-09-18 16:08:57 +0000

    Create contact.tsx

---
+++ apps/web/app/contact.tsx
@@ -0,0 +1,3 @@
+export default function Contact(){
+  return <div className="pt-28 max-w-3xl mx-auto px-6 text-white bg-black min-h-screen"><h1 className="text-4xl font-bold">Contact</h1><p className="mt-4">9 Road, Tema - +233 303 40 72 70</p></div>
+}
